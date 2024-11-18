76ba25b72a6eeeb8653f6b237363a6935a8c2aa6
olvrm <oliviermansuy7@gmail.com>
2024-11-18 02:07:02 -0500

    bug

--- src/__test__/endpoints.test.ts
+++ src/__test__/endpoints.test.ts
@@ -1,6 +1,9 @@
 import Dotenv from "dotenv";
 Dotenv.config({path: `${__dirname}/../.${process.env.NODE_ENV}.env`});
+import request from 'supertest';
 import app from '../app';
+
+jest.useFakeTimers();
 
 //expect(2 + 2).toBe(4);
 describe("Endpoints Test", () => {
@@ -8,43 +11,46 @@ describe("Endpoints Test", () => {
   describe("Authentication", () => {
 
     test("Admin login", async () => {
-      // const res = await request(app).post("/v2/login").send({ "username": "morrison@gmail.com", "password": "83r5^_" })
-      // expect(res).toBe();
+      const res = await request(app).post("/v2/login")
+      .send({ "username": "morrison@gmail.com", "password": "83r5^_" })
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      expect(res.statusCode).toBe(200);
     });
   
-    test("User signup (oli)", async () => {
+    // test("User signup (oli)", async () => {
   
-    });
+    // });
   
-    test("User login (oli)", async () => {
+    // test("User login (oli)", async () => {
   
-    });
+    // });
 
   });
 
-  describe("Products endpoints", () => {
+  // describe("Products endpoints", () => {
 
-    test("Get One", async () => {
+  //   test("Get One", async () => {
     
-    });
+  //   });
   
-    test("Save one", async () => {
+  //   test("Save one", async () => {
   
-    });
+  //   });
     
-    test("Products by category", async () => {
+  //   test("Products by category", async () => {
   
-    });
+  //   });
   
-    test("Products by stock attribute", async () => {
+  //   test("Products by stock attribute", async () => {
   
-    });
+  //   });
     
-    test("Products price range", async () => {
+  //   test("Products price range", async () => {
   
-    });
+  //   });
 
-  });
+  // });
 
 });
 
